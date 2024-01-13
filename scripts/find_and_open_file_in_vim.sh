fddd72c5701082b317daa32d9c287a3075e539f0
adxmantium <ajadams10@gmail.com>
2024-01-12 19:39:08 -0800

    fixing find and open in vim script

--- scripts/find_and_open_file_in_vim.sh
+++ scripts/find_and_open_file_in_vim.sh
@@ -4,13 +4,15 @@
 search_directories=(
 ~/Dev
 ~/scripts
+~/.dotfiles
 )
 
 # use find to find files in filtered list of dirs & store selection in a var
-selected=$(find "${search_directories[@]}" -type f | fzf-tmux -p --reverse --prompt="Select a file...")
+selected=$(find "${search_directories[@]}" -type f | fzf-tmux -p --reverse --prompt="Select file > ")
 
 # if selected, the open in vim
 if [ -n "$selected" ]; then
-  cd "$(dirname "$selected_file")"
   nvim "$selected"
+else
+  echo "no file selected"
 fi
