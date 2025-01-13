03997efc817e2863dcb076fc768e25d89020d51a
adxmantium <ajadams10@gmail.com>
2025-01-13 14:39:04 -0800

    feat: adding tpm & nvm/node install

--- install.sh
+++ install.sh
@@ -33,6 +33,29 @@ echo "Installing required packages..."
 # Core dependencies
 install_if_missing "stow"
 
+# Install TPM (tmux plugin manager)
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+if [ ! -d "$TPM_DIR" ]; then
+    echo "Installing tmux plugin manager..."
+    git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+fi
+
+# Install nvm (Node Version Manager) & node
+if [ ! -d "$HOME/.nvm" ]; then
+    echo "Installing nvm..."
+    LATEST_NVM=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_NVM/install.sh | bash
+
+    # Load nvm immediately for the rest of the script
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    
+    # Install latest Node.js
+    echo "Installing latest Node.js..."
+    nvm install node # "node" is an alias for the latest version
+    nvm alias default node # Set the latest version as default
+fi
+
 # Terminal and shell tools
 install_if_missing "wezterm" "cask"
 install_if_missing "font-meslo-lg-nerd-font" "cask"
